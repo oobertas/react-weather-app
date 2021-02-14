e1e06146a3e28682716666e31038efb0e8397abb
oobertas <65638737+oobertas@users.noreply.github.com>
2021-02-14 13:01:40 -0500

    Added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+        This project was coded by {" "}
+        <a href = 'https://www.linkedin.com/in/olha-obertas-77932b15b/'
+           target = '_blank'>
+           Olha Obertas
+           </a>
+           {" "}and is{" "} 
+      <a href = 'https://github.com/oobertas/react-weather-app'
+         target = '_blank'>
+        open-sourced on GitHub
+      </a>
+      </footer>
     </div>
   );
 }
