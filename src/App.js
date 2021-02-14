e3c20239ad562758374d8a74b0239ca2f2a0ea8d
oobertas <65638737+oobertas@users.noreply.github.com>
2021-02-14 15:00:15 -0500

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <div className = 'container'>
       Weather App
       <footer>
         This project was coded by {" "}
@@ -17,6 +18,7 @@ export default function App() {
         open-sourced on GitHub
       </a>
       </footer>
+    </div>
     </div>
   );
 }
